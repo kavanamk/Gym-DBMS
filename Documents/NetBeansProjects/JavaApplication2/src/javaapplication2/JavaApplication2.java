acf240d87bb1ae912fd531831365422c36fb3722
Admin <kavana.manvi@gmail.com>
2018-10-27 18:16:57 +0530

    equipment connected

---
+++ Documents/NetBeansProjects/JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+
+ */
+
+package javaapplication2;
+
+ import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class JavaApplication2 {
+    
+    //Connection method
+    
+    public static Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:F:/New folder/DB/New folder/DBMS.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+            System.out.println("Connection established");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+    
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //establish connection
+        JavaApplication2 obj = new JavaApplication2();
+        obj.connect();
+        
+        //open first jframe
+        NewJFrame2 obj1 = new NewJFrame2();
+        obj1.setVisible(true);
+        
+        //insert login
+ 
+        
+        }
+    
+}
